9e288c1bb318da6eee824c45fcfbd6584bb12c62
Igor Prokhorchuk <igor.prokhorchuk@yahoo.com>
2023-05-31 21:55:07 +0300

    feat: add vendor detail page

--- petnet/urls.py
+++ petnet/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from core.views import frontpage, about
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('store.urls')),
-    path('', frontpage, name='frontpage'),
     path('admin/', admin.site.urls),
     path('about/', about, name='about'),
-]
+    path('', include('userprofile.urls')),
+    path('', include('store.urls')),
+    path('', frontpage, name='frontpage'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
